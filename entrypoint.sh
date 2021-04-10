b065b169ce78161746b5a9615a9eaf6d9100eda3
qitas <qitas@qitas.cn>
2021-04-10 18:06:20 +0800

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,12 +8,12 @@ platformio platform install "https://github.com/OS-Q/P21.git" || {
   exit 1
 }
 # Prepare framework for CI
-# python3 -c "import json; import os; fp=open(os.path.expanduser('~/.qio/platforms/P21/link.json'), 'r+'); data=json.load(fp); data['packages']['A21A']['version'] = '*'; fp.seek(0); fp.truncate(); json.dump(data, fp); fp.close()" || {
-#   exit 1
-# }
-# ln --symbolic "$GITHUB_WORKSPACE" "$HOME/.qio/packages/A21A" || {
-#   exit 1
-# }
+python3 -c "import json; import os; fp=open(os.path.expanduser('~/.qio/platforms/P21/link.json'), 'r+'); data=json.load(fp); fp.seek(0); fp.truncate(); json.dump(data, fp); fp.close()" || {
+  exit 1
+}
+ln --symbolic "$GITHUB_WORKSPACE" "$HOME/.qio/packages/A21A" || {
+  exit 1
+}
 # Download and unpack CMSIS package
 wget --no-verbose "https://github.com/stm32duino/ArduinoModule-CMSIS/releases/download/$CMSIS_VERSION/$CMSIS_ARCHIVE" || {
   exit 1
