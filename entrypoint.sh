dafe9bbc0b1f75f6296f865a8faddec972a1a0ce
qitas <qitas@qitas.cn>
2021-04-10 16:55:50 +0800

    try

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,11 @@ readonly CMSIS_VERSION="$1"
 readonly CMSIS_ARCHIVE="CMSIS-${CMSIS_VERSION}.tar.bz2"
 
 # Install the development version of ststm32 platform
-platformio platform install "https://github.com/platformio/platform-ststm32.git" || {
+platformio platform install "https://github.com/OS-Q/P21.git" || {
   exit 1
 }
 # Prepare framework for CI
-python3 -c "import json; import os; fp=open(os.path.expanduser('~/.platformio/platforms/ststm32/platform.json'), 'r+'); data=json.load(fp); del data['packages']['framework-arduinoststm32']['owner']; fp.seek(0); fp.truncate(); json.dump(data, fp); fp.close()" || {
+python3 -c "import json; import os; fp=open(os.path.expanduser('~/.platformio/platforms/P21/platform.json'), 'r+'); data=json.load(fp); del data['packages']['framework-arduinoststm32']['owner']; fp.seek(0); fp.truncate(); json.dump(data, fp); fp.close()" || {
   exit 1
 }
 ln --symbolic "$GITHUB_WORKSPACE" "$HOME/.platformio/packages/framework-arduinoststm32" || {
