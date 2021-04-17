8480189f8e2c6e36a947434862bb575d7dfd6527
qitas <qitas@qitas.cn>
2021-04-17 17:34:18 +0800

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,9 @@
 
 readonly BOARD="$1"
 
-# Prepare framework for CI
-# python3 -c "import json; import os; fp=open(os.path.expanduser('~/.qio/platforms/P21/link.json'), 'r+'); data=json.load(fp); fp.seek(0); fp.truncate(); json.dump(data, fp); fp.close()" || {
-#   exit 1
-# }
-# ln --symbolic "$GITHUB_WORKSPACE" "$HOME/.qio/packages/A21A" || {
-#   exit 1
-# }
+python3 -c "import json; import os; fp=open(os.path.expanduser('~/.qio/platforms/P21/link.json'), 'r+'); data=json.load(fp); data['packages']['A21A']['optional'] = 'false'; fp.seek(0); fp.truncate(); json.dump(data, fp); fp.close()" || {
+  exit 1
+}
 
 cd "$HOME/.qio/packages/A21A" || {
   exit 1
