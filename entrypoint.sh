34797104fc8b78970888e7be3611afbdfc7005c4
qitas <qitas@qitas.cn>
2021-04-17 17:41:59 +0800

    try

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,19 @@
 
 readonly BOARD="$1"
 
-# python3 -c "import json; import os; fp=open(os.path.expanduser('~/.qio/platforms/P21/link.json'), 'r+'); data=json.load(fp); data['packages']['A21A']['optional'] = 'false'; fp.seek(0); fp.truncate(); json.dump(data, fp); fp.close()" || {
-#   exit 1
-# }
-
-cd "$HOME/.qio/packages/" || {
+python3 -c "import json; import os; fp=open(os.path.expanduser('~/.qio/platforms/P21/link.json'), 'r+'); data=json.load(fp); data['packages']['A21A']['optional'] = 'false'; fp.seek(0); fp.truncate(); json.dump(data, fp); fp.close()" || {
   exit 1
 }
 
-git clone --depth=3 https://github.com/OS-Q/A21A.git A21A
+pio update
+
+cd "$HOME/.qio/packages/A21A" || {
+  exit 1
+}
+
+git fetch
+
+# git clone --depth=3 https://github.com/OS-Q/A21A.git A21A
 
 cd "$GITHUB_WORKSPACE" || {
   exit 1
